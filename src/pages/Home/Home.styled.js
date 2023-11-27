8281519b2317533d9124946bac65a117e71d4a55
ElenaK <helenochka4@gmail.com>
2023-11-27 20:24:09 +0100

    Home

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -6,7 +6,10 @@ export const HomeImg = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-image: url('https://nft-assets.limewire.media/u/59b4484f-2de3-4566-afd2-3a5b8771a913/8dabd16b-1da3-4fe8-b187-472f8d05ac1b.d/post_image_large.jpeg?Expires=1700950376&Signature=yipo2LHkl8bLKOciWTfoe~1ulqq8blgWu3kzPiwjhXWlKQjGTCUwHy6gAUQKx4WJ8mBD7tqiv4seHbV3axZg376-R-w0I1mTP7a~6kxtAo1OH8KIafIX~wvHLVtH3pY4PiXb8IwSiRpFOa5~nuEyr2KVVg6~oE1B3eTm8xHh0EtBAwM2UyvgCR6MjwNfSqbp8eV8K-FDwUEzoEdQlMIAr3UXyLtx5j9w-g0C3Wue4ijfY7VI5TW83QWafSTADsX7KzkY~hT42LaJ1jb6PWeGktvsgMkc3zP24tRHYojXwPkz7~NVlw93lpOFjc~P-OsTqZ~5zAJNKqM7mM6CukmM4w__&Key-Pair-Id=KS8P5VE63SN9R');
+  background-image: url
+    (
+      'https://nft-assets.limewire.media/u/59b4484f-2de3-4566-afd2-3a5b8771a913/8dabd16b-1da3-4fe8-b187-472f8d05ac1b.d/post_image_large.jpeg?Expires=1700950376&Signature=yipo2LHkl8bLKOciWTfoe~1ulqq8blgWu3kzPiwjhXWlKQjGTCUwHy6gAUQKx4WJ8mBD7tqiv4seHbV3axZg376-R-w0I1mTP7a~6kxtAo1OH8KIafIX~wvHLVtH3pY4PiXb8IwSiRpFOa5~nuEyr2KVVg6~oE1B3eTm8xHh0EtBAwM2UyvgCR6MjwNfSqbp8eV8K-FDwUEzoEdQlMIAr3UXyLtx5j9w-g0C3Wue4ijfY7VI5TW83QWafSTADsX7KzkY~hT42LaJ1jb6PWeGktvsgMkc3zP24tRHYojXwPkz7~NVlw93lpOFjc~P-OsTqZ~5zAJNKqM7mM6CukmM4w__&Key-Pair-Id=KS8P5VE63SN9R'
+    );
   background-size: 880px;
   background-position: center;
   background-repeat: no-repeat;
